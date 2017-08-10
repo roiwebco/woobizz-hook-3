ee2519cfd37efe3440ceda1f97b1d15d0116dae9
roiwebco <30891172+roiwebco@users.noreply.github.com>
2017-08-10 23:20:49 +0200

    Initial version 1.0.0

---
+++ woobizz-hook-3.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Woobizz Hook 3 
+Plugin URI: http://woobizz.com
+Description: Disable emojis on wordpress
+Author: Woobizz
+Author URI: http://woobizz.com
+Version: 1.0.0
+Text Domain: woobizzhook3
+Domain Path: /lang/
+*/
+//Prevent direct acces
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+//Load translation
+add_action( 'plugins_loaded', 'woobizzhook3_load_textdomain' );
+function woobizzhook3_load_textdomain() {
+  load_plugin_textdomain( 'woobizzhook3', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
+}
+//Add Hook 3
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
